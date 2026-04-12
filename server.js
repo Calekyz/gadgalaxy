b7bfe2e23c7708fc3912348e15a02167f8f7e17e
Calekyz <caleborenge8@gmail.com>
2026-04-12 16:00:20 +0300

    Update server.js

--- server.js
+++ server.js
@@ -15,3 +15,6 @@ app.get('/api/orders', (req, res) => res.json(orders));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server on port ${PORT}`));
+app.get('/', (req, res) => {
+    res.send('GadgetGalaxy API is running');
+});
